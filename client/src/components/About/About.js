95fbcd6f1a6b39a26784a9c0815a1ba9ffc72697
rwahlst <axel.wahlstrom303@gmail.com>
2020-02-04 19:24:04 -0700

    The following has changed in this patch: + Added teammate names to our about page. + Added formatting for about page. + Added locations for bios.

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {Container, Row, Col, Button} from 'reactstrap';
 import '../tcowebstyle.css';
+import {CLIENT_TEAM_NAME} from "../Constants";
+
 
 export default class About extends Component {
 
@@ -10,7 +12,7 @@ export default class About extends Component {
           <Row>
             <Col>
               <h2 className="font-weight-bold" >
-                Hugh-Lit Pack-Herd
+                  {CLIENT_TEAM_NAME}
               </h2>
             </Col>
             <Col id="closeAbout" xs='auto' >
@@ -21,15 +23,50 @@ export default class About extends Component {
           </Row>
           <Row>
             <Col>
-                <p><strong>Our Values:</strong></p>
+                <h5><strong>Our Values:</strong></h5>
             </Col>
           </Row>
           <Row>
             <Col>
             <p><em><strong>
                 “Hugh-Lit Pack-Herd earns Dave Matthew's respect and loyalty by consistently providing the most average quality and value. We achieve sufficient-enough grades to finance growth, create value for our class mates and achieve our corporate objectives.”
-            </strong>-The Hugh-Lit Pack-Herd team (2020)</em></p>
+            </strong> -The Hugh-Lit Pack-Herd team (2020)</em></p>
             </Col>
+          </Row>
+          <Row>
+              <Col>
+                  <h5><strong>Meet the team:</strong></h5>
+                  <p>___________________________________</p>
+              </Col>
+          </Row>
+          <Row>
+              <Col>
+                  <h6><strong>Axel Wahlstrom</strong></h6>
+                  <img src={'src/components/About/resources/Empty.jpg'} alt={'src/components/About/resources/Empty.jpg'}/>
+                  <p><strong>My name is Axel Wahlstrom, I am a Junior Computer Science major at Colorado State University. I am also working on a minor in mathematics. In my free time I enjoy lifting weights and playing video games.</strong></p>
+              </Col>
+              <Col>
+                  <h6><strong>Ethan Liem</strong></h6>
+                  <img src={'src/components/About/resources/Empty.jpg'} alt={'src/components/About/resources/Empty.jpg'}/>
+                  <p><strong>Bio Here</strong></p>
+              </Col>
+          </Row>
+          <Row>
+              <Col>
+                <p>___________________________________</p>
+              </Col>
+          </Row>
+          <Row>
+                <Col>
+                    <h6><strong>Brandon Vasquez</strong></h6>
+                    <img src={'src/components/About/resources/Empty.jpg'} alt={'src/components/About/resources/Empty.jpg'}/>
+                    <p><strong>Bio Here</strong></p>
+                </Col>
+                <Col>
+                    <h6><strong>Moise Lacrete</strong></h6>
+                    <img src={'src/components/About/resources/Empty.jpg'} alt={'src/components/About/resources/Empty.jpg'}/>
+                    <p><strong>Bio Here</strong></p>
+                </Col>
           </Row>
 
 
