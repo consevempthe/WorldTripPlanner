bfdbdc4b53e4f66abb5810353c4fdd6434c057e7
Brandon Vasquez <brandon.vasquez67@yahoo.com>
2020-02-20 09:38:15 -0700

    added constructor to Request Distance class

--- server/src/main/java/com/tco/server/RequestDistance.java
+++ server/src/main/java/com/tco/server/RequestDistance.java
@@ -1,5 +1,7 @@
 package com.tco.server;
 
+import com.tco.misc.GreatCircleDistance;
+
 import java.util.Map;
 
 public class RequestDistance extends RequestHeader {
@@ -7,6 +9,13 @@ public class RequestDistance extends RequestHeader {
     Map<String, String> place2;
     Double earthRadius;
     Long distance;
+
+    public RequestDistance(Map<String,String> p1, Map<String,String> p2, Double earthRadius)
+    {
+        this.place1 = p1;
+        this.place2 = p2;
+        this.earthRadius = earthRadius;
+    }
 
     @Override
     public void buildResponse() {
