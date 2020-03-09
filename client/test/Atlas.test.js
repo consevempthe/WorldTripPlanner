bedeec58c6e530d65617986fe648549c317af51a
rwahlst <axel.wahlstrom303@gmail.com>
2020-03-09 17:54:34 -0600

    Initial Tests for Atlas.js

--- client/test/Atlas.test.js
+++ client/test/Atlas.test.js
@@ -1,18 +1,38 @@
-import './enzyme.config.js';
-import React from 'react';
-import {shallow} from 'enzyme'; // if need 'mount' add it here
+import './enzyme.config.js'
+import React from 'react'
+import {shallow/*, mount*/} from 'enzyme'
 
-import Atlas from '../src/components/Atlas/Atlas';
+import Atlas from "../src/components/Atlas/Atlas";
 
 function testInitialAppState() {
+    const atlas = shallow(<Atlas/>);
 
-  const app = shallow(<Atlas />);
+    // test markerPosition
+    let actualMarkerPos = atlas.state().markerPosition;
+    let expectedMarkerPos = null;
 
-  let actualMarkerPosition = app.state().markerPosition;
+    expect(actualMarkerPos).toEqual(expectedMarkerPos);
 
-  let expectedMarkerPosition = null;
+    // test client Location Services
+    let actualLocationServices = atlas.state().LocationServiceOn;
+    let expectedLocationServices = false;
 
-  expect(actualMarkerPosition).toEqual(expectedMarkerPosition);
+    expect(actualLocationServices).toEqual(expectedLocationServices);
+
+    // test mapBounds
+    let actualMapBounds = atlas.state().mapBounds;
+    let expectedMapBounds = null;
+
+    expect(actualMapBounds).toEqual(expectedMapBounds);
+
+    // test otherMarkerPositions & its length
+    let actualOtherMarkerPositions = atlas.state().otherMarkerPositions;
+    let length = actualOtherMarkerPositions.length;
+    let expectedOtherMarkerPositions = [];
+    let expectedLengthOtherMarkerPositions = 0;
+
+    expect(actualOtherMarkerPositions).toEqual(expectedOtherMarkerPositions);
+    expect(length).toEqual(expectedLengthOtherMarkerPositions);
 }
 
 test("Testing Atlas's Initial State", testInitialAppState);
