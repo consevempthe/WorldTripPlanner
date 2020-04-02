72816aefbb7efe3d7f03b433a7302a334763096c
Ethan Liem <liem.ethan@gmail.com>
2020-04-02 16:21:53 -0600

    Fixed cumulative distance

--- client/test/Trip.test.js
+++ client/test/Trip.test.js
@@ -1,4 +1,5 @@
 import './enzyme.config.js';
+import './setupJest'
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 
@@ -24,7 +25,7 @@ function testInitialState() {
         places: [],
         distances: []
     };
-
+    expect(initial.state().showLoadFileModal).toEqual(false);
     expect(initial.state().trip).toEqual(expectedState);
 }
 
@@ -61,6 +62,7 @@ function testAddPlace() {
 
     const place1 = {name: 'fort collins', latitude: '40', longitude: '-105'};
     const place2 = {name: 'boulder', latitude: '39.23', longitude: '-104.7'};
+    const place3 = {name: 'denver', latitude: '20', longitude: '-103.45'};
 
     add.instance().addPlace('fort collins', '40', '-105');
     expect(add.state().trip.places.length).toEqual(1);
@@ -69,6 +71,10 @@ function testAddPlace() {
     add.instance().addPlace('boulder', '39.23', '-104.7');
     expect(add.state().trip.places.length).toEqual(2);
     expect(add.state().trip.places[1]).toEqual(place2);
+
+    add.instance().changeStartPlace('denver', '20', '-103.45');
+    expect(add.state().trip.places.length).toEqual(2);
+    expect(add.state().trip.places[0]).toEqual(place3);
 }
 
 test("Testing add place", testAddPlace);
@@ -82,3 +88,30 @@ function testAddTitle() {
 }
 
 test("Testing add title", testAddTitle);
+
+function testCumulativeDistance() {
+    let cumulative = mount(<Trip/>);
+
+    const fakeState = {
+        requestType: "trip",
+        requestVersion: 3,
+        options: {title: "", earthRadius: '3959.0'},
+        places: [
+            {"id": "kaseda", "name": "Phantom Canyon Brewing Co", "municipality": "Colorado Springs", "latitude": "38.83418", "longitude": "-104.82497", "altitude": "6035"},
+            {"id": "lnarmour", "name": "Equinox Brewing", "municipality": "Fort Collins", "latitude": "40.586345", "longitude": "-105.075813", "altitude": "5003"}
+        ],
+        distances: [30, 40, 50, 60 , 70]
+    };
+
+    cumulative.setState({trip: fakeState});
+
+    expect(cumulative.state().trip.distances.length).toEqual(5);
+    expect(cumulative.state().trip.places.length).toEqual(2);
+    expect(cumulative.find('UncontrolledAlert').length).toEqual(1);
+    expect(cumulative.find('Table').length).toEqual(1);
+
+    expect(cumulative.instance().computeCumulativeDistance()).toEqual(250);
+
+}
+
+test("Testing distance and render", testCumulativeDistance);
