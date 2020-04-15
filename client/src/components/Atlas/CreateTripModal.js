cc4bd6b93e0b6b8aa9fe2e33d20af6ec5f313522
Brandon Vasquez <brandon.vasquez67@yahoo.com>
2020-04-15 14:40:24 -0600

    Added slider to Modal

--- client/src/components/Atlas/CreateTripModal.js
+++ client/src/components/Atlas/CreateTripModal.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import {Modal, ModalHeader, ModalBody} from 'reactstrap';
 import {Form, FormGroup, Input, Label} from 'reactstrap';
+import RangeSlider from 'react-bootstrap-range-slider';
+import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
 
 export default class CreateTripModal extends Component {
     constructor(props) {
@@ -9,6 +11,7 @@ export default class CreateTripModal extends Component {
         this.state = {
             showModal: false,
             title: "",
+            optLevel: 0,
         };
     }
 
@@ -40,6 +43,8 @@ export default class CreateTripModal extends Component {
                     <FormGroup>
                         <Label>Title:</Label>
                         {this.renderInputField(this.state.title, "Add a Title to Your Trip")}
+                        <Label>Optimize Trip</Label>
+                        {this.renderSliderScale()}
                     </FormGroup>
                 </Form>
             </ModalBody>
@@ -56,7 +61,21 @@ export default class CreateTripModal extends Component {
         )
     }
 
+    renderSliderScale() {
+        return (
+            <RangeSlider
+                value={this.state.optLevel}
+                max={60}
+                onChange={e => this.updateOptimizeLevel(Number(e.target.value))}
+            />
+        );
+    }
+
     updateTitle(value) {
         this.setState({title: value});
+    }
+
+    updateOptimizeLevel(value) {
+        this.setState({optLevel: value});
     }
 }
