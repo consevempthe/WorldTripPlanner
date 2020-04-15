93bbd7d0208bf8effc2d46420adac2fdf0b26322
Brandon Vasquez <brandon.vasquez67@yahoo.com>
2020-04-15 13:46:06 -0600

    Added inout field for title to modal

--- client/src/components/Atlas/CreateTripModal.js
+++ client/src/components/Atlas/CreateTripModal.js
@@ -1,24 +1,62 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import {Modal, ModalHeader, ModalBody} from 'reactstrap';
+import {Form, FormGroup, Input, Label} from 'reactstrap';
 
 export default class CreateTripModal extends Component {
     constructor(props) {
         super(props);
-        this.toggleModal = this.toggleModal.bind(this);
 
         this.state = {
             showModal: false,
+            title: "",
         };
     }
 
     render() {
         return(
             <div>
-
+                {this.renderCreateTripModal()}
             </div>
         )
     }
 
-    toggleModal() {
-        this.setState({showModal: !this.state.showModal});
+    renderCreateTripModal() {
+        return(
+            <div>
+                <Modal isOpen={this.props.isOpen} toggle={() => this.props.toggleModal}>
+                    <ModalHeader toggle={() => this.props.toggleModal()}>
+                        Create A Trip
+                    </ModalHeader>
+                    {this.renderForm()}
+                </Modal>
+            </div>
+        )
+    }
+
+    renderForm() {
+        return(
+            <ModalBody>
+                <Form>
+                    <FormGroup>
+                        <Label>Title:</Label>
+                        {this.renderInputField(this.state.title, "Add a Title to Your Trip")}
+                    </FormGroup>
+                </Form>
+            </ModalBody>
+        )
+    }
+
+    renderInputField(value, placeholder) {
+        return(
+            <Input
+                onChange={(e) => this.updateTitle(e.target.value)}
+                value={value}
+                placeholder={placeholder}
+            />
+        )
+    }
+
+    updateTitle(value) {
+        this.setState({title: value});
     }
 }
