5ad5054bcc7dd690f5676f601be4f124940f449c
Brandon Vasquez <brandon.vasquez67@yahoo.com>
2020-04-23 14:58:17 -0600

    Update CreateTripModal.test.js

--- client/test/CreateTripModal.test.js
+++ client/test/CreateTripModal.test.js
@@ -1,12 +1,25 @@
 import './enzyme.config';
 import './setupJest';
 import React from 'react';
-import {mount} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import CreateTripModal from '../src/components/Atlas/CreateTripModal';
+
+function testInitalState() {
+    const initial = shallow(<CreateTripModal/>);
+    let showModal = false;
+    let title = "";
+    let optLevel = 0;
+    expect(initial.state().showModal).toEqual(showModal);
+    expect(initial.state().title).toEqual(title);
+    expect(initial.state().optLevel).toEqual(optLevel);
+}
+
+test("Testing Create Trip Modal's Initial State", testInitalState);
 
 function testRender() {
     const createTripModal = mount(<CreateTripModal/>);
     expect(createTripModal.find('Modal').length).toEqual(1);
+    expect(createTripModal.find(''))
 }
 
 test("Testing Create Trip Modal's render", testRender);
