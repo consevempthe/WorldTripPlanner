79029b8f1611ae63c9dabde341bf129c3ac64e47
Ethan Liem <liem.ethan@gmail.com>
2020-05-06 12:01:53 -0600

    travis test

--- server/src/test/java/com/tco/server/TestRequestFind.java
+++ server/src/test/java/com/tco/server/TestRequestFind.java
@@ -58,44 +58,44 @@ public class TestRequestFind {
         System.out.println(countIsOne);
     }
 
-//    @Test
-//    public void testDatabaseFunctions() {
-//        String query_test = "denver";
-//        int limit_test = 40;
-//        Narrow filters = new Narrow();
-//
-//        String query = test.generateSQL(query_test, filters);
-//        String query_actual = String.format("Select * FROM world" +
-//                " WHERE name LIKE '%%%1$s%%'" +
-//                " OR iso_region LIKE '%%%1$s%%'" +
-//                " OR iso_country LIKE '%%%1$s%%'" +
-//                " OR municipality LIKE '%%%1$s%%';", query_test);
-//
-//        assertEquals(query, query_actual);
-//
-//        Place[] query_result = test.query(query_test, limit_test, filters);
-//        Integer query_count = test.getCOUNT();
-//        Integer expected_Count = isTravis ? 0 : 30;
-//        assertEquals(expected_Count, query_count);
-//        int expected_Size = isTravis ? 0 : 30;
-//        assertEquals(expected_Size, query_result.length);
-//
-////        for(Place location : query_result) {
-////            System.out.println(location.getPlace());
-////        }
-//
-//    }
+    @Test
+    public void testDatabaseFunctions() {
+        String query_test = "denver";
+        int limit_test = 40;
+        Narrow filters = new Narrow();
 
-//    @Test
-//    public void runRequestFind() {
-//        test_query.buildResponse();
-//        assertEquals(Integer.valueOf(4), test_query.getLimit());
-//        Integer actual_count = test_query.getFound();
-//        Integer expected = isTravis ? 0 : 2;
-//        assertEquals(expected, actual_count);
-//
-//        Place[] actual_places = test_query.getPlaces();
-//        int length = isTravis ? 0 : 2;
-//        assertEquals(length, actual_places.length);
-//    }
+        String query = test.generateSQL(query_test, filters);
+        String query_actual = String.format("Select * FROM world" +
+                " WHERE name LIKE '%%%1$s%%'" +
+                " OR iso_region LIKE '%%%1$s%%'" +
+                " OR iso_country LIKE '%%%1$s%%'" +
+                " OR municipality LIKE '%%%1$s%%';", query_test);
+
+        assertEquals(query, query_actual);
+
+        Place[] query_result = test.query(query_test, limit_test, filters);
+        Integer query_count = test.getCOUNT();
+        Integer expected_Count = isTravis ? 26 : 30;
+        assertEquals(expected_Count, query_count);
+        int expected_Size = isTravis ? 26 : 30;
+        assertEquals(expected_Size, query_result.length);
+
+//        for(Place location : query_result) {
+//            System.out.println(location.getPlace());
+//        }
+
+    }
+
+    @Test
+    public void runRequestFind() {
+        test_query.buildResponse();
+        assertEquals(Integer.valueOf(4), test_query.getLimit());
+        Integer actual_count = test_query.getFound();
+        Integer expected = isTravis ? 1 : 2;
+        assertEquals(expected, actual_count);
+
+        Place[] actual_places = test_query.getPlaces();
+        int length = isTravis ? 1 : 2;
+        assertEquals(length, actual_places.length);
+    }
 }
